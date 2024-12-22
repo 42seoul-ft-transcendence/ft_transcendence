81e19216381f0a8bcc5180cf4fa102b5c7390772
Seokkyu Lee <cd1004s@gmail.com>
2024-12-22 20:28:53 +0900

    fix: urls

--- backend/transcendence/transcendence/urls.py
+++ backend/transcendence/transcendence/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls')),
-    path('api/', include('friendship.urls')),
-    path('api/', include('game.urls')),
+    path('login/', include('authentication.urls')),
+    path('friendship/', include('friendship.urls')),
+    path('game/', include('game.urls')),
 ]
