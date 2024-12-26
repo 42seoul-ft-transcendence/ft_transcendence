e51313f6f316ce3501f67b651c2d49fdcbf46f1b
sumssumm <sumssum.p@gmail.com>
2024-12-26 15:50:51 +0900

    Frontend: loading page css

--- frontend/src/pages/LoadingView.js
+++ frontend/src/pages/LoadingView.js
@@ -2,13 +2,13 @@ import Component from "../core/Component.js";
 import TwoFAView from "./TwoFAView.js";
 
 export default class Loading extends Component {
-  template() {
-    return /* html */ `
-			<div class="loading">
+	template() {
+		return /* html */ `
+			<div class="loading d-flex justify-content-center align-items-center">
 				<div class="spinner-border text-primary" role="status">
 					<span class="visually-hidden">Loading...</span>
 				</div>
 			</div>
 		`;
-  }
+	}
 }
